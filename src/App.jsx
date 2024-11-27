b8c2cb55231ef5dd69ed22a99f28fc9d053293fa
muthomi100 <techeasesolns@gmail.com>
2024-11-27 23:18:27 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListing from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListing />
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default App;
